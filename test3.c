ac93c513fc4fe4497fe828b84f0af68031d22ab2
Han1Seo1hyun <hyun0529hsh@gmail.com>
2024-12-13 22:42:36 +0900

    C프1 소스파일

---
+++ test3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("---------------------------------------------\n");
+	printf("            학과: 소프트웨어융합학과            \n"); 
+	printf("            학번: 20241060            \n");
+	printf("            성명: 한서현            \n");
+	printf("---------------------------------------------");
+	return 0;
+}
